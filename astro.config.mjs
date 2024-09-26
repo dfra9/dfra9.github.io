a2e70af65119c10ada81c401b6676770bbdca142
dfra9 <defara41@gmail.com>
2024-09-26 19:22:36 +0700

    feat: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dfra9/dfra9.github.io',
   integrations: [tailwind()],
 });
